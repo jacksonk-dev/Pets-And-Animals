ec6396984b9e0f341cd6d0ce2d80e81cfdc54579
jacksonk-dev <jacksonk.dev@gmail.com>
2022-02-19 00:44:36 +0300

    Activates routing and adds icons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PetsComponent } from './pets/pets.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'pets', component: PetsComponent },
+  { path: 'roast', component: PetsComponent },
+  { path: 'animals', component: PetsComponent },
+  { path: 'animal-feeds', component: PetsComponent },
+  { path: 'vet-services', component: PetsComponent },
+  { path: 'settings', component: PetsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PetsComponent } from './pets/pets.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PetsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
