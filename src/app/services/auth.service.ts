15c215c3f0c5c9a7934d78d1b8004f493313e77a
jacksonk-dev <jacksonk.dev@gmail.com>
2022-04-04 11:57:51 +0300

    A little house keeping

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -15,6 +15,7 @@ export class AuthService {
   loginWithGoogle() {
     return signInWithPopup(this.auth, new GoogleAuthProvider())
       .then((res) => {
+        console.log(res)
         this.user = {
           ...res.user.providerData[0],
           isLoggedIn: true
